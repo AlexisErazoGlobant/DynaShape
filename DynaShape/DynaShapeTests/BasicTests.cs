14dd550ebdc487a55970bf3cc9876258f57ff73b
Long Nguyen <longnguyen.connect@gmail.com>
2024-09-02 19:43:44 +0700

    Fixed bug in LengthGoalTest

--- DynaShape/DynaShapeTests/BasicTests.cs
+++ DynaShape/DynaShapeTests/BasicTests.cs
@@ -15,12 +15,13 @@ public class BasicTests
     public void LengthGoalTest()
     {
         Solver solver = new Solver();
-        LengthGoal lengthGoal = new LengthGoal(new Triple(0f, 0f, 0f), new Triple(2f, 0f, 0f), 0f);
+        LengthGoal lengthGoal = new LengthGoal(new Triple(0f, 0f, 0f), new Triple(2f, 0f, 0f), 0f, 1000f);
         solver.RegisterGoal(lengthGoal);
-        solver.Iterate(10);
+        solver.EnableMomentum = false;
+        solver.Iterate();
         List<Triple> nodePositions = solver.GetNodePositions();
         float xCoordinate = nodePositions[0].X;
-        Assert.AreEqual(xCoordinate, 0f);
+        Assert.AreEqual(xCoordinate, 1f);
         Assert.Pass();
     }
 }
